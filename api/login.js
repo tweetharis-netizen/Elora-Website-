6a7c83e8b21fa54b633bf02b5799397e3f7520e2
tweetharis-netizen <tweetharis@gmail.com>
2025-12-30 16:16:55 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,5 @@
 module.exports = async function handler(req, res) {
   res.statusCode = 410;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({
-    ok: false,
-    error: "Deprecated. Use /api/verification/* endpoints."
-  }));
+  res.end(JSON.stringify({ ok: false, error: "Deprecated endpoint." }));
 };
