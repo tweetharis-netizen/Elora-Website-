1456834e57e0432f5dc6a4097dd36e035dd2a634
tweetharis-netizen <tweetharis@gmail.com>
2026-01-07 21:50:32 +0800

    Delete api/login.js

--- api/login.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.statusCode = 410;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: false, error: "Deprecated endpoint." }));
-};
